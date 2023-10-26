e0ff04f5217b79e9f62ad849ed9666b04f29e40a
tycoonz <jabroochoudhary@gmail.com>
2023-10-26 22:48:38 +0500

    postgre added

--- api/views.py
+++ api/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
-# Create your views here.
+
+def recommendation_songs(req):
+    if req.method == 'GET':
+        print("Post Request")
+        data_response = {"msg":  "Valid request type." , "error":False,}
+        return JsonResponse(data=data_response)
+    else:
+        return JsonResponse(data= {"msg":  "Invalid request type." , "error":True,},)
+    
+
+def get_predicted_songs_list():
+    predicted_songs = []
+    return predicted_songs
+
+def get_recent_song_list(user_id):
+    recents_songs = []
+    return recents_songs
+
+def get_newly_added_songs_list():
+    new_songs = []
+    return new_songs
+
